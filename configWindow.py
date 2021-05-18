502e55b6e159e7d05fc3f2c86cf25633ec56f5f8
Jacques Grelet <jacques.grelet@gmail.cpm>
2021-05-18 09:52:30 +0200

    fix bug toml location works with main

--- configWindow.py
+++ configWindow.py
@@ -17,17 +17,17 @@ class ConfigWindow(QWidget):
 
         self.version = version
          # setup toml configuration file
-        self.config = Path(path.expandvars('$APPDATA/' + appName)).with_suffix('.toml')
-        if not path.isfile(self.config):
-            print(f"Configuration file don't exist, create one from default config to {self.config}")
+        self.configFile = Path(path.expandvars('$APPDATA/' + appName)).with_suffix('.toml')
+        if not path.isfile(self.configFile):
+            print(f"Configuration file don't exist, create one from default config to {self.configFile}")
             self.saveDefaultConfig()
-        self.__cfg = toml.load(self.config)
+        self.__cfg = toml.load(self.configFile)
         if not "version" in self.__cfg or self.__cfg["version"] != version:
             self.saveDefaultConfig()
-            self.__cfg = toml.load(self.config)
+            self.__cfg = toml.load(self.configFile)
 
         # Create the stacked layout
-        self.stackedLayout = QStackedLayout()
+        #self.stackedLayout = QStackedLayout()
 
        # overloading operators
     def __getitem__(self, key):
@@ -39,7 +39,7 @@ class ConfigWindow(QWidget):
         else:
             return self.__cfg[key]
         
-    def globalConfiguration(self):
+    def displayGlobalConfig(self):
     
         # create the configuration panel
         self.config = QWidget()
@@ -50,14 +50,14 @@ class ConfigWindow(QWidget):
         screen_height = QLineEdit(str(self.frameGeometry().height()))
         originCombo = QComboBox()
         originCombo.addItems(["bottom", "surface"])
-        index = originCombo.findText(self.cfg['config']['origin'], Qt.MatchFixedString)
+        index = originCombo.findText(self.__cfg['config']['origin'], Qt.MatchFixedString)
         if index >= 0:
              originCombo.setCurrentIndex(index)
         # connect signal to function selectOrigin, pass argument with functools.partial
         screen_width.textEdited.connect(partial(self.selectScreenWidth, screen_width))
         #screen_width.textEdited.connect(self.selectScreenWidth)
         originCombo.activated.connect(partial(self.selectOrigin, originCombo))
-        bottom_depth = QLineEdit(str(self.cfg['config']['bottom_depth']))
+        bottom_depth = QLineEdit(str(self.__cfg['config']['bottom_depth']))
         formLayout.addRow("Screen width", screen_width)
         formLayout.addRow("Screen height", screen_height)
         formLayout.addRow("Origin", originCombo)
@@ -74,25 +74,25 @@ class ConfigWindow(QWidget):
         dlgLayout.addWidget(btnBox)
         self.config.setLayout(dlgLayout)
  
-        self.stackedLayout.addWidget(self.config)
+        #self.stackedLayout.addWidget(self.config)
         #self.setCentralWidget(self.config)
 
     def selectScreenWidth(self, width):
         print(f"New width: {width.text()}")
-        self.cfg['global']['screen_width'] = width.text()
+        self.__cfg['global']['screen_width'] = width.text()
 
     def selectOrigin(self, comboBox):
         print(f"Origin Selected: {comboBox.currentText()}")
-        self.cfg['config']['origin'] = comboBox.currentText()
+        self.__cfg['config']['origin'] = comboBox.currentText()
 
     def acceptConfig(self):
-        print(f"{self.cfg['global']['screen_width']} x {self.cfg['global']['screen_height']}")
+        print(f"{self.__cfg['global']['screen_width']} x {self.__cfg['global']['screen_height']}")
 
     def cancelConfig(self):
         print("Configuration cancelled...")
     
     def saveDefaultConfig(self):
-        with open(self.config, 'w') as fid:
+        with open(self.configFile, 'w') as fid:
             cfg = ConfigWindow.getDefaultConfig()
             cfg['version'] = self.version
             toml.dump(cfg, fid)
@@ -124,9 +124,11 @@ if __name__ == "__main__":
     # Create the application
     app = QApplication([])
 
-    cfg = ConfigWindow(__file__, "1.0") 
+    appName = Path(__file__).with_suffix('').stem
+    cfg = ConfigWindow(appName, "1.0")
+    cfg.displayGlobalConfig() 
 
-    cfg.show()
+    cfg.config.show()
 
     # Run the event loop
     app.exec_()
