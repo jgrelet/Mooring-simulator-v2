b62719e15cfd041b4be8471b9bd02fc4f988d8a1
Jacques Grelet <jacques.grelet@ird.fr>
2021-10-05 16:42:56 +0200

    add double linked list and test

---
+++ tools/test_dll.py
@@ -0,0 +1,43 @@
+# test_sentinel_DLL.py
+# CS 1 class example by THC.
+# Tests the Sentinel_DLL class.
+
+from dll import Sentinel_DLL
+
+def test_sentinel_DLL():
+    # Make a linked list with Maine, Idaho, and Utah.
+    l = Sentinel_DLL()
+    l.append("Maine")
+    l.append("Idaho")
+    l.append("Utah")
+    print(l)
+
+    # Add Ohio after Idaho.
+    node = l.find("Idaho")
+    if node != None:
+        print(node.get_data())
+        l.insert_after(node, "Ohio")
+    print(l)
+
+    # Delete Idaho.
+    if node != None:
+        l.delete(node)
+    print(l)
+
+    # Add Ohio after Idaho.
+    node = l.find("Ohio")
+    if node != None:
+        print(node.get_data())
+        l.insert_after(node, "Paris")
+    print(l)
+
+    l.prepend("Nantes")
+    print(l)
+
+    # Empty out the list, one node at a time.
+    while l.first_node() != None:
+        l.delete(l.first_node())
+
+    print(l)
+
+test_sentinel_DLL()
