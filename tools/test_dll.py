3ee7c283cf5f12095e794269c5609b46adea374b
Jacques Grelet <jacques.grelet@gmail.cpm>
2021-10-18 10:06:42 +0200

    add mooring items for testing

--- tools/test_dll.py
+++ tools/test_dll.py
@@ -44,7 +44,32 @@ def test_sentinel_DLL():
     node = l.find("Brest")
     if node != None:
         print(node.get_data())
-        l.insert_after(node, {'Float','45'})
+        l.insert_after(node, {
+        "Terminals": {
+            "1": {
+                "attribute": 0.0,
+                "category": "Category",
+                "name": "Name",
+                "mass": "Mass (kg, Wet)",
+                "length": "Length (m)",
+                "projected_area": "Projected area (m\u00b2)",
+                "nl_drag_cf": "Normal drag coeff",
+                "tl_drag_cf": "Tangential drag coeff",
+                "breaking_strength": "Breaking strength (kg)",
+                "image_file": "Image file"
+            },
+            "2": {
+                "attribute": 1.0,
+                "category": "LPO",
+                "name": "Shackle 5/8",
+                "mass": -0.635,
+                "length": 0.05,
+                "projected_area": 0.001,
+                "nl_drag_cf": 1.3,
+                "tl_drag_cf": 0.1,
+                "breaking_strength": 18000.0,
+                "image_file": "\\Pictures\\Terminals\\shackle2.bmp"
+        }}})
     print(l)
 
 
